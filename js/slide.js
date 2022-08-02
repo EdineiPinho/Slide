a353e74c24d9b17f0d6fc354cc057bfc745670e0
edineipinho <edneyrock@gmail.com>
2022-08-02 02:36:45 -0300

    feat(style.css, script.js): adicionada funcionalidade que concerta o slide após o resize da tela

--- js/slide.js
+++ js/slide.js
@@ -1,11 +1,14 @@
-export default class Slide {
+import debounce from './debounce.js';
+
+export class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide)
     this.wrapper = document.querySelector(wrapper);
     this.dist = { finalPosition: 0, startX: 0, movement: 0 }
+    this.activeClass = 'active';
   }
 
-  transition(active){
+  transition(active) {
     this.slide.style.transition = active ? 'transform .3s' : '';
   }
 
@@ -43,20 +46,20 @@ export default class Slide {
     const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
     this.wrapper.removeEventListener(movetype, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
-    this.changeSlideOnEnd();
     this.transition(true);
+    this.changeSlideOnEnd();
   }
 
-  changeSlideOnEnd(){
-    if(this.dist.movement > 120 && this.index.next!== undefined){
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
       this.activeNextSlide();
-    } else if(this.dist.movement < -120 && this.index.prev!== undefined){
+    } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
       this.activePrevSlide();
     } else {
       this.changeSlide(this.index.active);
     }
   }
-  
+
   addSlideEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart);
     this.wrapper.addEventListener('touchstart', this.onStart);
@@ -64,27 +67,22 @@ export default class Slide {
     this.wrapper.addEventListener('touchend', this.onEnd);
   }
 
-  bindEvents() {
-    this.onStart = this.onStart.bind(this);
-    this.onMove = this.onMove.bind(this);
-    this.onEnd = this.onEnd.bind(this);
-  }
-
   // Slides config
-  slidePosition(slide){
+
+  slidePosition(slide) {
     const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
     return -(slide.offsetLeft - margin);
   }
-  
-  slidesConfig(){
+
+  slidesConfig() {
     this.slideArray = [...this.slide.children].map((element) => {
-      const poistion = this.slidePosition(element);
+      const position = this.slidePosition(element);
       return { position, element };
     });
   }
 
-  slideIndexNav(index){
-    const last = this.slideArray.length -1;
+  slidesIndexNav(index) {
+    const last = this.slideArray.length - 1;
     this.index = {
       prev: index ? index - 1 : undefined,
       active: index,
@@ -92,26 +90,69 @@ export default class Slide {
     }
   }
 
-  changeSlide(index){
+  changeSlide(index) {
     const activeSlide = this.slideArray[index];
-    this.moveSlide(activeSlide.position)
-    this.slideIndexNav(index);
+    this.moveSlide(activeSlide.position);
+    this.slidesIndexNav(index);
     this.dist.finalPosition = activeSlide.position;
+    this.changeActiveClass();
   }
 
-  activePrevSlide(){
-    if(this.index.prev !== undefined) this.changeSlide(this.index.prev)
+  changeActiveClass() {
+    this.slideArray.forEach(item => item.element.classList.remove(this.activeClass));
+    this.slideArray[this.index.active].element.classList.add(this.activeClass);
   }
-  
-  activeNextSlide(){
-    if(this.index.next !== undefined) this.changeSlide(this.index.next)
+
+  activePrevSlide() {
+    if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined) this.changeSlide(this.index.next);
+  }
+
+  onResize() {
+    setTimeout(() => {
+      this.slidesConfig();
+      this.changeSlide(this.index.active);
+    }, 1000);
+  }
+
+  addResizeEvent() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+
+    this.activePrevSlide = this.activePrevSlide.bind(this);
+    this.activeNextSlide = this.activeNextSlide.bind(this);
+
+    this.onResize = debounce(this.onResize.bind(this), 200);
   }
 
   init() {
     this.bindEvents();
+    this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
-    this.transition(true);
+    this.addResizeEvent();
+    this.changeSlide(0);
     return this;
+  }
+}
+
+export class SlideNav extends Slide {
+  addArrow(prev, next) {
+    this.prevElement = document.querySelector(prev);
+    this.nextElement = document.querySelector(next);
+    this.addArrowEvent();
+  }
+
+  addArrowEvent() {
+    this.prevElement.addEventListener('click', this.activePrevSlide);
+    this.nextElement.addEventListener('click', this.activeNextSlide);
   }
 }
